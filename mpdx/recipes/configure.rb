3a5ab796f6e662efbd4a97e2c09ea237011f08c6
Josh Starcher <josh.starcher@gmail.com>
2013-04-24 22:17:06 -0400

    Move mpdx to it's own folder

---
+++ mpdx/recipes/configure.rb
@@ -0,0 +1,21 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  template "#{deploy[:deploy_to]}/shared/config/config.yml" do
+    source "config.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:config => deploy[:config], :environment => deploy[:rails_env])
+
+    notifies :run, resources(:execute => "restart Rails app #{application}")
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
+end

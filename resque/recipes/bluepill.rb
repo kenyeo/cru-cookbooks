930d4e231a6f67e04926e7592777548a23ea5900
Josh Starcher <josh.starcher@gmail.com>
2013-04-29 20:59:25 -0400

    run bluepill config during resque restart

--- resque/recipes/bluepill.rb
+++ resque/recipes/bluepill.rb
@@ -12,6 +12,4 @@ node[:deploy].each do |application, deploy|
 
   execute "touch /var/log/bluepill.log; chown #{deploy[:user]}:#{deploy[:group]} /var/log/bluepill.log"
 
-  resque_config
-
 end
--- resque/recipes/restart.rb
+++ resque/recipes/restart.rb
@@ -3,9 +3,12 @@ include_recipe "deploy"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
+  resque_config
+
   execute "restart resque for #{application}" do
     cwd deploy[:current_path]
     command "bluepill restart resque"
     action :nothing
   end
+
 end

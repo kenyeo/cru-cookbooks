2fa3b41edc123b8003ed1819b66ae9b60854adab
Josh Starcher <josh.starcher@gmail.com>
2013-04-30 16:45:25 -0400

    Got definition format wrong

--- resque/recipes/restart.rb
+++ resque/recipes/restart.rb
@@ -3,7 +3,12 @@ include_recipe "deploy"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  resque_config path: deploy[:deploy_to], owner: deploy[:user], group: deploy[:group], bundler: true
+  resque_config do
+    path deploy[:deploy_to]
+    owner deploy[:user]
+    group deploy[:group]
+    bundler true
+  end
 
   execute "restart resque for #{application}" do
     cwd deploy[:current_path]

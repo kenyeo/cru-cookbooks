bb76d0f88b514e24903a1cec24cabc3723e4223b
Josh Starcher <josh.starcher@gmail.com>
2013-05-02 12:59:32 -0400

    Add asset compilation to missionhub::configure

--- missionhub/recipes/configure.rb
+++ missionhub/recipes/configure.rb
@@ -2,13 +2,6 @@ include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
 
-
-  execute "restart Rails app #{application}" do
-    cwd deploy[:current_path]
-    command node[:opsworks][:rails_stack][:restart_command]
-    action :nothing
-  end
-
   template "#{deploy[:deploy_to]}/shared/config/config.yml" do
     source "config.yml.erb"
     cookbook 'missionhub'
@@ -23,5 +16,29 @@ node[:deploy].each do |application, deploy|
       File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
     end
   end
+
+
+  Chef::Log.info("Running deploy/before_migrate.rb...")
+
+  Chef::Log.info("Symlinking #{release_path}/public/assets to #{new_resource.deploy_to}/shared/assets")
+
+  directory "#{new_resource.deploy_to}/shared/assets" do
+    action :create
+  end
+
+  link "#{release_path}/public/assets" do
+    to "#{new_resource.deploy_to}/shared/assets"
+  end
+
+  rails_env = new_resource.environment["RAILS_ENV"]
+  Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+
+  execute "rake assets:precompile" do
+    cwd release_path
+    command "bundle exec rake assets:precompile"
+    environment "RAILS_ENV" => rails_env
+    action :run
+  end
+
 
 end

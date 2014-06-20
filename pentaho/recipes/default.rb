5cd162f468bb1b706b1ab2d7e7eaa6df512cc68b
Cesar <cesar.torres@cru.org>
2014-06-20 10:48:21 -0400

    Update default.rb

--- pentaho/recipes/default.rb
+++ pentaho/recipes/default.rb
@@ -5,7 +5,7 @@ bash "install_program" do
     wget http://sourceforge.net/projects/pentaho/files/Data%20Integration/4.1.0-stable/pdi-ce-4.1.0-stable.tar.gz -O /tmp/pdi-ce-4.1.0-stable.tar.gz
     mkdir /opt/pentaho
     tar -zxf /tmp/pdi-ce-4.1.0-stable.tar.gz -C /opt/pentaho
-    chmod +x /opt/pentaho/*.sh
+    chmod +x /opt/pentaho/data-integration/*.sh
   EOH
 end
 

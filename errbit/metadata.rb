e02dde338735ecc8b664034ff8688666a3f2c299
Cesar <cesar.torres@cru.org>
2014-07-10 16:15:36 -0400

    Update metadata.rb

--- errbit/metadata.rb
+++ errbit/metadata.rb
@@ -5,3 +5,9 @@ license          'Apache 2.0'
 description      'Installs/Configures errbit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "mongodb"
+depends "git"
+depends "build-essential"
+depends "apache2"
+depends "passenger_apache2"

18e6003aa3145653528192402988630fb4f69fd1
Josh Starcher <josh.starcher@gmail.com>
2014-07-15 15:34:04 -0400

    Try to install haproxy 1.5

---
+++ haproxy/recipes/default.rb
@@ -0,0 +1,34 @@
+execute "add-apt-repository ppa:vbernat/haproxy-1.5 -y" do
+  user "root"
+end
+
+execute "apt-get update" do
+  user "root"
+end
+
+package 'haproxy' do
+  action :install
+end
+
+if platform?('debian','ubuntu')
+  template '/etc/default/haproxy' do
+    source 'haproxy-default.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
+end
+
+include_recipe 'haproxy::service'
+
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :restart, "service[haproxy]"
+end
+
+service 'haproxy' do
+  action [:enable, :start]
+end

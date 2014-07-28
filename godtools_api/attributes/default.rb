5f5d1eb19acbfd75d5ed4637513ec9569ed890a7
ryancarlson <ryan.t.carlson@cru.org>
2014-07-28 13:45:37 -0400

    add errbit properties

--- godtools_api/attributes/default.rb
+++ godtools_api/attributes/default.rb
@@ -4,3 +4,6 @@ default['godtools']['database_url'] = 'jdbc:postgresql://10.7.196.222/godtools'
 default['godtools']['database_username'] = 'godtoolsuser'
 
 default['godtools']['onesky_api_key'] = 'JSlA3a3tTcixcpEegcympv3HQCJB7q1H'
+
+default['crs-api']['errbit-endpoint'] = 'https://errors.uscm.org/notifier_api/v2/notices'
+default['crs-api']['errbit-environment'] = 'production'

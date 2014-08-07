b2475c0ec207df01d3edaaeeafa1c1e7c3e024b3
Justin Sabelko <justin.sabelko@cru.org>
2014-08-07 16:02:52 -0500

    Might help to get rid of the rails check

--- deploy/recipes/atlassian.rb
+++ deploy/recipes/atlassian.rb
@@ -3,11 +3,6 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
   next if node[:current_app] && node[:current_app] != application
 
-  if deploy[:application_type] != 'rails'
-    Chef::Log.debug("Skipping deploy::rails application #{application} as it is not a Rails app")
-    next
-  end
-
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]

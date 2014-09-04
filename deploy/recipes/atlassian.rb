021a6ec015b172d2269635385fa9a5178f76b357
Justin Sabelko <justin.sabelko@cru.org>
2014-09-04 10:51:59 -0500

    Don't create install directory

--- deploy/recipes/atlassian.rb
+++ deploy/recipes/atlassian.rb
@@ -1,12 +1,6 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
-
   opsworks_atlassian do
     deploy_data deploy
     app application

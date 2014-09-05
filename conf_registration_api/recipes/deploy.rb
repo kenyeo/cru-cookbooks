ee196286b2020cc513627c6ca261b25421591ceb
ryancarlson <ryan.t.carlson@cru.org>
2014-09-05 10:40:35 -0400

    Remove method and just create variables in execute

--- conf_registration_api/recipes/deploy.rb
+++ conf_registration_api/recipes/deploy.rb
@@ -1,23 +1,19 @@
-
-def json
+execute 'deploy-from-jenkins' do
   environment = {
-    :name => 'targetEnvironment', :value => node['crs-api']['continuous-integration']['environment']
+      :name => 'targetEnvironment', :value => node['crs-api']['continuous-integration']['environment']
   }
   ip = {
-    :name => 'targetServerIP', :value => node['opsworks']['instance']['private_ip']
+      :name => 'targetServerIP', :value => node['opsworks']['instance']['private_ip']
   }
   password = {
-    :name => 'deployerPassword', :value => node['wildfly']['deploy-password']
+      :name => 'deployerPassword', :value => node['wildfly']['deploy-password']
   }
   database_migration = {
-    :name => 'databaseMigration', :value => node['crs-api']['continuous-integration']['database-migration']
+      :name => 'databaseMigration', :value => node['crs-api']['continuous-integration']['database-migration']
   }
 
+  json = Hash.new()
   json['parameter'] = Array.(environment,ip,password,database_migration)
 
-end
-
-execute 'deploy-from-jenkins' do
-  puts(json)
   command 'curl -X POST ' + node['crs-api']['continuous-integration']['build-url'] + ' -d token=' + node['crs-api']['continuous-integration']['api-token'] + ' --data-urlencode json="' + json + '"'
 end

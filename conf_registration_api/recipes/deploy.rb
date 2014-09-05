0bfc9faba160462d643ede3c286d6043c6a5fe24
ryancarlson <ryan.t.carlson@cru.org>
2014-09-05 10:19:31 -0400

    Add a deploy recipe for ERT

---
+++ conf_registration_api/recipes/deploy.rb
@@ -0,0 +1,23 @@
+
+def json
+  environment = {
+    :name => 'targetEnvironment', :value => node['crs-api']['continuous-integration']['environment']
+  }
+  ip = {
+    :name => 'targetServerIP', :value => node['opsworks']['instance']['private_ip']
+  }
+  password = {
+    :name => 'deployerPassword', :value => node['wildfly']['deploy-password']
+  }
+  database_migration = {
+    :name => 'databaseMigration', :value => node['crs-api']['continuous-integration']['database-migration']
+  }
+
+  json['parameter'] = Array.(environment,ip,password,database_migration)
+
+  puts(json)
+end
+
+execute 'deploy-from-jenkins' do
+  command 'curl -X POST ' + node['crs-api']['continuous-integration']['build-url'] + ' -d token=' + node['crs-api']['continuous-integration']['api-token'] + ' --data-urlencode json="' + json + '"'
+end

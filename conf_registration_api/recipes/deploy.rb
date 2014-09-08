84d9c7e6772bc48899d387bb9644458a54a02cd7
ryancarlson <ryan.t.carlson@cru.org>
2014-09-08 10:13:10 -0400

    remove comment

--- conf_registration_api/recipes/deploy.rb
+++ conf_registration_api/recipes/deploy.rb
@@ -3,8 +3,6 @@ chef_gem 'jenkins_api_client'
 ruby_block 'deploy-from-jenkins' do
   require 'jenkins_api_client'
 
-  # these variables have not been checked against the stack settings!!
-  
   @client = JenkinsApi::Client.new(:server_ip => node['crs-api']['continuous_integration']['hostname'],
                                    :username => node['crs-api']['continuous_integration']['username'],
                                    :password => node['crs-api']['continuous_integration']['password'])

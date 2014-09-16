c7f342bdbf4216802736dc06a9b129e9ee7dddc7
Justin Sabelko <justin.sabelko@cru.org>
2014-09-16 14:07:45 -0500

    Fix not_if clause

--- atlassian/recipes/service.rb
+++ atlassian/recipes/service.rb
@@ -8,11 +8,11 @@ node[:deploy].each do |application, deploy|
       :application => application,
       :deploy => deploy
     )
-    not_if deploy[:no_service]
+    not_if { deploy[:no_service] }
   end
 
   service application do
     action [:enable, :start]
-    not_if deploy[:no_service]
+    not_if { deploy[:no_service] }
   end
 end

9b4ddf0634c6543b0c165187875f43afb3f680a2
Cesar.Torres <cesar.torres>
2014-11-17 14:50:33 -0500

    New Relic Plugin's Cookbook

---
+++ newrelic_aws_cloudwatch_plugin-master/lib/newrelic_aws/collectors.rb
@@ -0,0 +1,13 @@
+require "time"
+require "aws-sdk"
+
+require "newrelic_aws/collectors/base"
+require "newrelic_aws/collectors/ec2"
+require "newrelic_aws/collectors/ebs"
+require "newrelic_aws/collectors/elb"
+require "newrelic_aws/collectors/rds"
+require "newrelic_aws/collectors/ddb"
+require "newrelic_aws/collectors/sqs"
+require "newrelic_aws/collectors/sns"
+require "newrelic_aws/collectors/ec"
+require "newrelic_aws/collectors/ecr"

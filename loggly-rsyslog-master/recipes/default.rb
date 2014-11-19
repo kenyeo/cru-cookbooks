3d1c0a2a0aa19b655ba7774b5d6cbad65aee56c4
Cesar.Torres <cesar.torres>
2014-11-19 11:35:31 -0500

    Adding Loggly

---
+++ loggly-rsyslog-master/recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: loggly
+# Recipe:: default
+#
+# Copyright (C) 2014 Matt Veitas
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if node['loggly']['token']['from_databag']
+  databag = node['loggly']['token']['databag']
+  databag_item = node['loggly']['token']['databag_item']
+
+  loggly_token = Chef::EncryptedDataBagItem.load(databag, databag_item)['token']
+  raise "No token was found in databag item: #{databag}/#{databag_item}" if loggly_token.nil?
+else
+  raise "When not using a Data Bag, you have to define the Loggly token manually" if node['loggly']['token']['value'].empty?
+  loggly_token = node['loggly']['token']['value']
+end
+
+include_recipe "rsyslog::default"
+
+include_recipe "loggly-rsyslog::tls" if node['loggly']['tls']['enabled']
+
+template node['loggly']['rsyslog']['conf'] do
+  source 'rsyslog-loggly.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  variables({
+    :monitor_files => !node['loggly']['log_files'].empty? || !node['loggly']['log_dirs'].empty?,
+    :tags => node['loggly']['tags'].nil? || node['loggly']['tags'].empty? ? '' : "tag=\\\"#{node['loggly']['tags'].join("\\\" tag=\\\"")}\\\"",
+    :token => loggly_token
+  })
+  notifies :restart, "service[rsyslog]", :immediate
+end
